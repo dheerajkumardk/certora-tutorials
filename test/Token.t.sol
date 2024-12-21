ed41825f737edb0bb63ae5ff33572ac30209f4e6
dheerajkumardk <dyaduwanshi0@gmail.com>
2024-12-21 13:18:17 +0530

    add MYToken contract and test

---
+++ test/Token.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/src/Test.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract TokenTest is Test {
+    MyToken token;
+    
+    function setUp() public {
+        token = new MyToken();
+    }
+
+    function testTokenSetup() public {
+        assertEq(token.name(),"My Token");
+        assertEq(token.symbol(), "MTN");
+    }
+}
